b6fe240669a065e6100d000ee71393c3e2d2a435
EvilDragon <mario.kruselj@gmail.com>
2025-06-09 16:34:30 +0200

    Tweak formatting and verbiage of file header copyright info (#46)

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -1,27 +1,23 @@
 /*
- * OB-Xf - a continuation of the last open source version
- * of OB-Xd.
+ * OB-Xf - a continuation of the last open source version of OB-Xd.
  *
- * OB-Xd was originally written by Filatov Vadim, and
- * then a version was released under the GPL3
- * at https://github.com/reales/OB-Xd. Subsequently
- * the product was continued by DiscoDSP and the copyright
- * holders as an excellent closed source product. For more
- * see "HISTORY.md" in the root of this repo.
+ * OB-Xd was originally written by Vadim Filatov, and then a version
+ * was released under the GPL3 at https://github.com/reales/OB-Xd.
+ * Subsequently, the product was continued by DiscoDSP and the copyright
+ * holders as an excellent closed source product. For more info,
+ * see "HISTORY.md" in the root of this repository.
  *
- * This repository is a successor to the last open source
- * release, a version marked as 2.11. Copyright
- * 2013-2025 by the authors as indicated in the original
- * release, and subsequent authors per the github transaction
- * log.
+ * This repository is a successor to the last open source release,
+ * a version marked as 2.11. Copyright 2013-2025 by the authors
+ * as indicated in the original release, and subsequent authors
+ * per the GitHub transaction log.
  *
- * OB-Xf is released under the Gnu General Public Licence
- * V3 or later (GPL-3.0-or-later). The license is found in the file
- * "LICENSE" in the root of this repository or at
+ * OB-Xf is released under the GNU General Public Licence v3 or later
+ * (GPL-3.0-or-later). The license is found in the file "LICENSE"
+ * in the root of this repository or at:
  * https://www.gnu.org/licenses/gpl-3.0.en.html
  *
- * All source for OB-Xf is available at
- * https://github.com/surge-synthesizer/OB-Xf
+ * Source code is available at https://github.com/surge-synthesizer/OB-Xf
  */
 
 #ifndef OBXF_SRC_PLUGINPROCESSOR_H
