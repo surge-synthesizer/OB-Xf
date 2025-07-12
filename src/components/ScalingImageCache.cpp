72fae1c3984eefc0d1134a3928d6834a09bcc57a
Joe <152509645+joeloftusdev@users.noreply.github.com>
2025-07-12 15:14:03 +0100

    update header guards and comments (#227)

--- src/components/ScalingImageCache.cpp
+++ src/components/ScalingImageCache.cpp
@@ -1,3 +1,25 @@
+/*
+ * OB-Xf - a continuation of the last open source version of OB-Xd.
+ *
+ * OB-Xd was originally written by Vadim Filatov, and then a version
+ * was released under the GPL3 at https://github.com/reales/OB-Xd.
+ * Subsequently, the product was continued by DiscoDSP and the copyright
+ * holders as an excellent closed source product. For more info,
+ * see "HISTORY.md" in the root of this repository.
+ *
+ * This repository is a successor to the last open source release,
+ * a version marked as 2.11. Copyright 2013-2025 by the authors
+ * as indicated in the original release, and subsequent authors
+ * per the GitHub transaction log.
+ *
+ * OB-Xf is released under the GNU General Public Licence v3 or later
+ * (GPL-3.0-or-later). The license is found in the file "LICENSE"
+ * in the root of this repository or at:
+ * https://www.gnu.org/licenses/gpl-3.0.en.html
+ *
+ * Source code is available at https://github.com/surge-synthesizer/OB-Xf
+ */
+
 #include "ScalingImageCache.h"
 #include <juce_core/juce_core.h>
 #include <juce_gui_basics/juce_gui_basics.h>
--- src/components/ScalingImageCache.h
+++ src/components/ScalingImageCache.h
@@ -1,4 +1,27 @@
-#pragma once
+/*
+ * OB-Xf - a continuation of the last open source version of OB-Xd.
+ *
+ * OB-Xd was originally written by Vadim Filatov, and then a version
+ * was released under the GPL3 at https://github.com/reales/OB-Xd.
+ * Subsequently, the product was continued by DiscoDSP and the copyright
+ * holders as an excellent closed source product. For more info,
+ * see "HISTORY.md" in the root of this repository.
+ *
+ * This repository is a successor to the last open source release,
+ * a version marked as 2.11. Copyright 2013-2025 by the authors
+ * as indicated in the original release, and subsequent authors
+ * per the GitHub transaction log.
+ *
+ * OB-Xf is released under the GNU General Public Licence v3 or later
+ * (GPL-3.0-or-later). The license is found in the file "LICENSE"
+ * in the root of this repository or at:
+ * https://www.gnu.org/licenses/gpl-3.0.en.html
+ *
+ * Source code is available at https://github.com/surge-synthesizer/OB-Xf
+ */
+
+#ifndef OBXF_SRC_COMPONENTS_SCALINGIMAGECACHE_H
+#define OBXF_SRC_COMPONENTS_SCALINGIMAGECACHE_H
 #include <juce_graphics/juce_graphics.h>
 #include <unordered_map>
 #include <map>
@@ -26,3 +49,5 @@ struct ScalingImageCache
     static constexpr std::array<int, 3> zoomLevels = {100, 200, 400};
     int baseZoomLevel = 100;
 };
+
+#endif // OBXF_SRC_COMPONENTS_SCALINGIMAGECACHE_H
