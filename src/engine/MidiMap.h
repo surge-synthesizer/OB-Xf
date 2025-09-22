ecd69447b2b9b9aafb6905ab9c68425e40118375
EvilDragon <mario.kruselj@gmail.com>
2025-09-22 14:38:26 +0200

    Clean up MidiMap.h (#359)

--- src/engine/MidiMap.h
+++ src/engine/MidiMap.h
@@ -26,149 +26,38 @@
 #include "SynthEngine.h"
 #include "juce_audio_basics/juce_audio_basics.h"
 
+static constexpr uint8_t NUM_MIDI_CC = 128;
+
 class MidiMap
 {
   public:
-    int controllers[255]{};
-    int controllers_default[255]{};
+    std::array<int, NUM_MIDI_CC> controllers{};
 
     std::map<juce::String, int> mapping;
 
     bool loaded = false;
-    MidiMap()
-    {
-        reset();
-        set_default();
-    }
 
-    void reset()
-    {
-        for (int i = 0; i < 255; i++)
-        {
-            controllers[i] = -1;
-            controllers_default[i] = -1;
-        }
-    }
+    MidiMap() { reset(); }
 
-    void set_default()
-    {
-        auto getParaIdByName = [](const std::string &name) -> int {
-            for (const auto &i : ParameterList)
-                if (i.meta.name == name)
-                    return i.meta.id;
-            return -1;
-        };
-
-        struct MappingEntry
-        {
-            int cc;
-            const std::string &name;
-        };
-
-        using namespace SynthParam::Name;
-
-        // clang-format off
-        const MappingEntry entries[] =
-        {
-            {71, Volume},
-            {15, Polyphony},
-            {33, Tune},
-            {17, Transpose},
-            {118, BendUpRange},
-            {34, Osc2Detune},
-            {35, EnvLegatoMode},
-            {75, LFO1Rate},
-            {76, FilterEnvAmount},
-            {20, VelToFilterEnv},
-            {21, NotePriority},
-            {23, Portamento},
-            {16, Unison},
-            {24, UnisonDetune},
-            {43, Osc2Detune},
-            {44, LFO1Wave1},
-            {45, LFO1Wave2},
-            {46, LFO1Wave3},
-            {22, LFO1ModAmount1},
-            {25, LFO1ModAmount2},
-            {47, LFO1ToOsc1Pitch},
-            {48, LFO1ToOsc2Pitch},
-            {49, LFO1ToFilterCutoff},
-            {50, LFO1ToOsc1PW},
-            {51, LFO1ToOsc2PW},
-            {52, OscSync},
-            {53, OscCrossmod},
-            {54, Osc1Pitch},
-            {55, Osc2Pitch},
-            {56, FilterEnvInvert},
-            {57, Osc1SawWave},
-            {58, Osc1PulseWave},
-            {59, Osc2SawWave},
-            {60, Osc2PulseWave},
-            {61, OscPW},
-            {62, OscBrightness},
-            {63, EnvToPitchAmount},
-            {77, Osc1Mix},
-            {78, Osc2Mix},
-            {102, NoiseMix},
-            {103, FilterKeyFollow},
-            {74, FilterCutoff},
-            {42, FilterResonance},
-            {104, FilterMode},
-            {18, FilterSlop},
-            {105, Filter2PoleBPBlend},
-            {106, Filter4PoleMode},
-            {73, AmpEnvAttack},
-            {36, AmpEnvDecay},
-            {37, AmpEnvSustain},
-            {72, AmpEnvRelease},
-            {38, FilterEnvAttack},
-            {39, FilterEnvDecay},
-            {40, FilterEnvSustain},
-            {41, FilterEnvRelease},
-            {108, EnvelopeSlop},
-            {109, FilterSlop},
-            {110, PortamentoSlop},
-            {81, PanVoice1},
-            {82, PanVoice2},
-            {83, PanVoice3},
-            {84, PanVoice4},
-            {85, PanVoice5},
-            {86, PanVoice6},
-            {87, PanVoice7},
-            {88, PanVoice8},
-            {113, EnvToPWAmount},
-            {114, EnvToPWBothOscs},
-            {115, EnvToPitchBothOscs},
-            {117, Osc2PWOffset},
-            {118, LevelSlop},
-            {119, Filter2PolePush}
-        };
-        // clang-format on
-
-        for (const auto &entry : entries)
-        {
-            int paraId = getParaIdByName(entry.name);
-            controllers[entry.cc] = controllers_default[entry.cc] = paraId;
-            mapping[entry.name] = paraId;
-        }
-    }
+    void reset() { controllers.fill(-1); }
 
     int &operator[](int index)
     {
-        if (index >= 255)
+        if (index >= NUM_MIDI_CC)
         {
             exit(0);
         }
+
         return controllers[index];
     }
 
     void setXml(juce::XmlElement &xml)
     {
-        for (int i = 0; i < 255; ++i)
+        for (auto c : controllers)
         {
-            if (controllers[i] != -1)
+            if (c > -1)
             {
-                xml.setAttribute("MIDI_" + juce::String(i), getTag(controllers[i]));
+                xml.setAttribute("CC" + juce::String(c), getTag(c));
             }
         }
     }
@@ -183,39 +72,29 @@ class MidiMap
                 return it->first;
             }
         }
-        return "undefine";
+
+        return "undefined";
     }
 
     int getParaId(juce::String tagName) { return mapping[tagName]; }
 
     void getXml(juce::XmlElement &xml)
     {
-        for (int i = 0; i < 255; ++i)
+        for (auto &c : controllers)
         {
-            juce::String tmp = xml.getStringAttribute("MIDI_" + juce::String(i), "undefine");
-            if (tmp != "undefine")
-            {
-                controllers[i] = getParaId(tmp);
-            }
-        }
+            juce::String tmp = xml.getStringAttribute("CC" + juce::String(c), "undefined");
 
-        // Backward keys
-        if (controllers[100] > 0)
-        {
-            controllers[77] = controllers_default[77];
-            controllers[100] = 0;
-        }
-        if (controllers[101] > 0)
-        {
-            controllers[78] = controllers_default[78];
-            controllers[101] = 0;
+            if (tmp != "undefined")
+            {
+                c = getParaId(tmp);
+            }
         }
     }
 
     bool loadFile(juce::File &xml)
     {
         reset();
-        set_default();
+
         if (xml.existsAsFile())
         {
             juce::XmlDocument xmlDoc(xml);
@@ -228,13 +107,14 @@ class MidiMap
 
     void updateCC(int idx_para, int midiCC)
     {
-        for (int i = 0; i < 255; i++)
+        for (auto &c : controllers)
         {
-            if (controllers[i] == idx_para)
+            if (c == idx_para)
             {
-                controllers[i] = -1;
+                c = -1;
             }
         }
+
         controllers[midiCC] = idx_para;
     }
 
@@ -244,10 +124,10 @@ class MidiMap
         this->setXml(ele);
 
         if (auto outStream = xml.createOutputStream())
+        {
             ele.writeTo(*outStream);
+        }
     }
-
-    void clean() { reset(); }
 };
 
 #endif // OBXF_SRC_ENGINE_MIDIMAP_H
